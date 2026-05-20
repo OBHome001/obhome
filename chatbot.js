80f7eb0cdcc2fa2d75a97f3d86b431a83925fd23
Petchalak Khomsornpan <petchy@Petchalaks-MacBook-Pro.local>
2026-05-20 10:33:43 +0700

    d

--- chatbot.js
+++ chatbot.js
@@ -290,7 +290,7 @@
       'กฎ: ถ้าถามราคาให้บอกจากข้อมูลด้านบน ถ้าไม่มีให้แนะนำโทรถามร้าน ห้ามเดาราคา'
     ].join('\n');
 
-    fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' + GEMINI_KEY, {
+    fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + GEMINI_KEY, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
